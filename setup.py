231c93713be6ff1a9f5444e9e47b58af04aed701
Christian Wiegand <cwiegand@wgdnet.de>
2021-01-03 20:15:48 +0100

    Change version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='django-bootstrap-icons',
-    version='0.1.0',
+    version='0.1.1',
     packages=setuptools.find_packages(),
     include_package_data=True,
     description='A quick way to add Bootstrap Icons with Django template tags.',
