2a210cede855abd1ea49561b447c4cef28c1aa12
Christian Wiegand <cwiegand@wgdnet.de>
2021-04-16 14:22:13 +0200

    Move duplicated code to function

--- django_bootstrap_icons/templatetags/bootstrap_icons.py
+++ django_bootstrap_icons/templatetags/bootstrap_icons.py
@@ -5,6 +5,7 @@ import xml.dom.minidom
 
 from django.conf import settings
 from django.contrib.staticfiles.finders import find
+# noinspection PyProtectedMember
 from django.template import Library
 from django.utils.html import format_html
 
@@ -28,72 +29,58 @@ def get_static(icon_name):
         )
 
 
+def render_svg(content, size, color, extra_classes):
+    svg = content.getElementsByTagName('svg')
+
+    if extra_classes:
+        svg[0].attributes['class'].value += ' ' + extra_classes
+    if size:
+        svg[0].attributes['width'].value = size
+        svg[0].attributes['height'].value = size
+    if color:
+        svg[0].attributes['fill'].value = color
+
+    return content.toprettyxml()
+
+
 @register.simple_tag
 def custom_icon(icon_name, size=None, color=None, extra_classes=None):
+    """
+    Template tag for rendering a custom icon
+    :param str icon_name: Name of custom icon to render
+    :param str size: size of custom icon to render
+    :param str color: color of custom icon to render
+    :param str extra_classes: String of classes to add to icon
+    """
     if icon_name is None:
         return ''
-    try:
-        icon_path = get_static(icon_name)
-        content = xml.dom.minidom.parse(icon_path)
-        svg = content.getElementsByTagName('svg')
 
-        if extra_classes:
-            svg[0].attributes['class'].value += ' '+extra_classes
-        if size:
-            svg[0].attributes['width'].value = size
-            svg[0].attributes['height'].value = size
-        if color:
-            svg[0].attributes['fill'].value = color
-
-        result = content.toprettyxml()
-    except:
-        result=''
-        import traceback
-        traceback.print_exc()
-
-    return format_html(result)
+    icon_path = get_static(icon_name)
+    content = xml.dom.minidom.parse(icon_path)
+    return format_html(render_svg(content, size, color, extra_classes))
 
 
 @register.simple_tag
 def bs_icon(icon_name, size=None, color=None, extra_classes=None):
     """
-        Template tag for rendering a bootstrap icon
-        :param str icon_name: Name of bootstrap icon to render
-        :param str size: size of bootstrap icon to render
-        :param str color: color of bootstrap icon to render
-        :param str extra_classes: String of classes to add to icon
-        """
+    Template tag for rendering a bootstrap icon
+    :param str icon_name: Name of bootstrap icon to render
+    :param str size: size of bootstrap icon to render
+    :param str color: color of bootstrap icon to render
+    :param str extra_classes: String of classes to add to icon
+    """
     if icon_name is None:
         return ''
 
-    try:
-        base_url = getattr(
-            settings,
-            'BS_ICONS_BASE_URL',
-            'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/'
-        )
-        icon_path = os.path.join(
-            base_url, 'icons', '.'.join((icon_name, 'svg'))
-        )
+    base_url = getattr(
+        settings,
+        'BS_ICONS_BASE_URL',
+        'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/'
+    )
+    icon_path = os.path.join(
+        base_url, 'icons', '.'.join((icon_name, 'svg'))
+    )
 
-        resp = requests.get(icon_path)
-        content = xml.dom.minidom.parseString(resp.text)
-        svg = content.getElementsByTagName('svg')
-
-        if extra_classes:
-            svg[0].attributes['class'].value += ' '+extra_classes
-        if size:
-            svg[0].attributes['width'].value = size
-            svg[0].attributes['height'].value = size
-        if color:
-            svg[0].attributes['fill'].value = color
-
-        result = content.toprettyxml()
-    except:
-        result=''
-        import traceback
-        traceback.print_exc()
-
-    return format_html(result)
-
-
+    resp = requests.get(icon_path)
+    content = xml.dom.minidom.parseString(resp.text)
+    return format_html(render_svg(content, size, color, extra_classes))
