df267fc850f8bf56d14750aa1549af8aa1e7ce23
Christian Wiegand <christianwgd@users.noreply.github.com>
2021-12-29 17:00:34 +0100

    Include sample app in repo

--- django_bootstrap_icons_sample/settings.py
+++ django_bootstrap_icons_sample/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'django_bootstrap_icons_sample/static')
+print(STATIC_ROOT)
 STATIC_URL = '/static/'
 
 # Default primary key field type
